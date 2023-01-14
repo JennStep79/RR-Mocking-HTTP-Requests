935da14c3332ac738b9441dca8cdce4e5ce72850
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-01-14 13:30:58 -0800

    add tests for header and link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
+  const [gitHubName, setGitHubName] = useState('')
+  const [gitHubUrl, setGitHubUrl] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/JennStep79')
+    .then(res => res.json())
+    .then(data => {
+        setGitHubName(data.type)
+        setGitHubUrl(data.html_url)
+    })
+}, [])
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
+      <a href={gitHubUrl}>Go to Profile</a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,26 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
+
+beforeEach(() => {
+  // re-initialize data
+  fetch.resetMocks()
+})
+
+test("GitHub api name renders to screen", async () => {
+  fetch.mockResponseOnce(JSON.stringify({ type: "User" }))
+  render(<App />)
+  const headerEl = await waitFor(() => screen.getByRole("heading", { level: 2 }))
+  expect(headerEl).toHaveTextContent("User")
+})
+
+test("button links to GitHub profile", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ html_url: "https://github.com/JennStep79" }))
+    render(<App />)
+    const linkEl = await waitFor(() => screen.getByRole("link"))
+    expect(linkEl).toHaveAttribute("href", "https://github.com/JennStep79")
+  })
+  
+
 
 
 
